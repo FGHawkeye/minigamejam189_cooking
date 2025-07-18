61eaa1f30decbd61b663edbc4a41077b33a435a2
FGHawkeye <33209411+FGHawkeye@users.noreply.github.com>
2025-07-18 20:37:23 -0300

    se instancian los ingredientes

--- scripts/generadorIngredientes.gd
+++ scripts/generadorIngredientes.gd
@@ -1,3 +1,23 @@
 extends Node
 
-@export var puntosAparicion: Array[Marker2D] = []
+@export var spawnPoints: Array[Marker2D] = []
+var foodScene = preload("res://scenes/ingrediente.tscn")
+
+func _ready() -> void:
+	getAllNodes(self)
+	spawnFood()
+
+func spawnFood():
+	for marker in spawnPoints:
+		var object = foodScene.instantiate()
+		call_deferred("add_child",object)
+		object.global_position = marker.global_position
+		
+
+func getAllNodes(node):
+	for N in node.get_children():
+		if N.get_child_count() > 0:
+			print("["+N.get_name()+"]")
+			getAllNodes(N)
+		else:
+			spawnPoints.append(N)
