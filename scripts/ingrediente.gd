82bba5b7dc81343a63d9ff6648011b66fcacfd91
Amunet-Ipi <ivanam.quinteros@gmail.com>
2025-07-19 00:49:11 -0300

    arreglo

--- scripts/ingrediente.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-func _ready():
-	var numIngr
-	numIngr = int(randf_range(1,9))
-	$LabelIngr.text = str(numIngr)
-	print("Valor del ingrediente:", numIngr)
-	get_node(".../root/scoreManager").sumar_puntaje(numIngr)
